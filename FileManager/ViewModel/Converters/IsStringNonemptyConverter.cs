64360813c7bec719985041d7d42e7972c74e19b6
Vika <samitova@gmail.com>
2017-01-11 15:30:11 +0200

    refresh

---
+++ FileManager/ViewModel/Converters/IsStringNonemptyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FileManager.ViewModel.Converters
+{
+    internal sealed class IsStringNonemptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !String.IsNullOrEmpty(value as string);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
